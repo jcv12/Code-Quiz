182d3b8f0bd19127522cf45be43829b7cc49aac6
jcv12 <john.c.vayianos@gmail.com>
2022-03-26 14:19:39 -0400

    adding time and score

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-// buttons
-
-const startBtn = document.querySelector("#start");
+// time and score
+let timeEl = document.querySelector("p.time");
+let secondsLeft = 75;
+let scoreEl = document.querySelector("#score");
