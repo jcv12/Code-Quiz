fec28926cb9369c023c2944ec3913a2369e2dc03
jcv12 <john.c.vayianos@gmail.com>
2022-03-27 02:18:44 -0400

    added timer function

--- script.js
+++ script.js
@@ -83,3 +83,18 @@ const questions = [ // array of objects
         correctAnswer: "3"
     }
 ];
+
+// timer
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+            questionsEl.style.display = "none";
+            finalEl.style.display = "block";
+            scoreEl.textContent = secondsLeft;
+        }
+    }, 1000);
+}
